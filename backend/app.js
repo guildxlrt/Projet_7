e9a1dc2c70d66eb2382d0a4ef6a3ed9e873604e5
Guillaume <guillaume.lauret@protonmail.com>
2022-09-21 22:08:36 +0200

    update ~ helmet express-rate-limit express-slow-down

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,50 @@
 //================//IMPORTS//================//
 const express = require('express');
-const cors = require('cors');
 const path = require('path');
 require('dotenv').config()
+// securite
+const cors = require('cors');
+const helmet = require("helmet");
+const rateLimit = require('express-rate-limit')
+const slowDown = require("express-slow-down");
+
+
+//================//APPLICATION//================//
 
 //========//References
 const app = express();
-app.use(express.json());
 const userRoutes = require('./routes/user');
 const postRoutes = require('./routes/post');
 const commentRoutes = require('./routes/comment');
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
+const speedLimiter = slowDown({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    delayAfter: 100, // allow 100 requests per 15 minutes, then...
+    delayMs: 500 // begin adding 500ms of delay per request above 100:
+    // request # 101 is delayed by  500ms
+    // request # 102 is delayed by 1000ms
+    // request # 103 is delayed by 1500ms
+    // etc.
+  });
+
+//========//START
+app.use(express.json());
 app.use(cors());
+app.use(helmet());
+// expresss rate limit
+app.use(limiter);
+// express slow down
+//app.enable("trust proxy"); // only if you're behind a reverse proxy (Heroku, Bluemix, AWS if you use an ELB, custom Nginx setup, etc)
+app.use(speedLimiter);
 
-//================//CONFIG//================//
-
-//========//Autorisation
+//========//Autorisations
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');

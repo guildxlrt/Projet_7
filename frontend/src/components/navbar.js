e58f50e7fa39d2aa58df6c25d9b252cd1310bed5
Guillaume <guillaume.lauret@protonmail.com>
2022-09-30 15:22:38 +0200

    update : 30-sept. #2 config redux

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -2,9 +2,12 @@ import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
 import { UidContext } from "./appContext";
 import Logout from "./Log/logout";
+import {useSelector} from 'react-redux'
 
 const Navbar = () => {
     const uid = useContext(UidContext)
+    const userData = useSelector((state) => state.userReducer)
+
   return (
     <nav>
         <div className="nav-container">
@@ -21,7 +24,7 @@ const Navbar = () => {
                     <li></li>
                     <li className="welcome">
                         <navlink exact to="profil">
-                            <h5>bienvenue 'valeur dynamique'</h5>
+                            <h5>Bienvenue {userData.surname}</h5>
                         </navlink>
                     </li>
                     <Logout />

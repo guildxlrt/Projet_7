6e8ee5f4b3ee16cc8957ba188e3c989e36b8dd66
Guillaume <guillaume.lauret@protonmail.com>
2022-10-06 04:47:27 +0200

    update_5 oct #3

--- frontend/src/components/UserProfil/UpdateBirthday.js
+++ frontend/src/components/UserProfil/UpdateBirthday.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { updateBirthday } from '../../actions/user.actions';
 
-
 const UpdateBirthday = () => {
     const userData = useSelector((state) => state.userReducer)
     const dispatch = useDispatch();
